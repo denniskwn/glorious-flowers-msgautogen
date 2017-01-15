cccbdcf2df2a7ab97770ec52acedea45ede9ca20
Dennis Kwan <denniskwn@gmail.com>
2017-01-15 20:59:50 +0800

    UI update

--- helloworld.php
+++ helloworld.php
@@ -2,6 +2,7 @@
   <!-- [START csslink] -->
   <head>
     <link type="text/css" rel="stylesheet" href="/stylesheets/main.css" />
+    <link rel="apple-touch-icon" href="https://instagram.fhkg1-1.fna.fbcdn.net/t51.2885-19/s320x320/12724935_1037070149698001_2086130098_a.jpg" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.16/clipboard.min.js"></script>
     <script>
         new Clipboard('.btn');
@@ -19,13 +20,14 @@
     $date2->add(new DateInterval('P18D'));
 
     $dateRngStr = $date1->format('Y-m-d')." 至 ".$date2->format('Y-m-d');
-    echo htmlspecialchars("謝謝你！製作完成後將會收到通知。14-18日後會寄出 ()謝謝你！製作完成後將會收到通知。<br />14-18日後會寄出 (".
+    $msg = htmlspecialchars("謝謝你！製作完成後將會收到通知。<br />14-18日後會寄出 (".
     $dateRngStr .")");
 
+    echo $msg;
     ?>
-
+<br />
     <!-- Trigger -->
-    <button class="btn" data-clipboard-text="Just because you can doesn't mean you should — clipboard.js">
+    <button class="btn" data-clipboard-text="<?php echo $msg; ?>">
         Copy to clipboard
     </button>
   </body>
