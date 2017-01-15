04766cae7a09a46e2810ba48eed9fdf920d63860
Dennis Kwan <denniskwn@gmail.com>
2017-01-15 21:13:15 +0800

    bugfixes for clipboard instantiation.

--- helloworld.php
+++ helloworld.php
@@ -4,9 +4,7 @@
     <link type="text/css" rel="stylesheet" href="/stylesheets/main.css" />
     <link rel="apple-touch-icon" href="https://instagram.fhkg1-1.fna.fbcdn.net/t51.2885-19/s320x320/12724935_1037070149698001_2086130098_a.jpg" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.16/clipboard.min.js"></script>
-    <script>
-        new Clipboard('.btn');
-    </script>
+
 
   </head>
   <!-- [END csslink] -->
@@ -20,7 +18,7 @@
     $date2->add(new DateInterval('P18D'));
 
     $dateRngStr = $date1->format('Y-m-d')." 至 ".$date2->format('Y-m-d');
-    $msg = "謝謝你！製作完成後將會收到通知。<br />14-18日後會寄出 (". $dateRngStr .")";
+    $msg = "謝謝你！製作完成後將會收到通知。\n14-18日後會寄出 (". $dateRngStr .")";
 
     echo $msg;
     ?>
@@ -34,5 +32,9 @@
         <button class="btn" data-clipboard-action="cut" data-clipboard-target="#bar">
          Cut to clipboard
         </button>
+
+        <script>
+            new Clipboard('.btn');
+        </script>
     </body>
 </html>
